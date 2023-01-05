31f5e382b7b48669b5ac96879157e09b8e53a284
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-01-05 13:55:19 +0300

    portfolio start

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './Main.module.css';
+
+const Main = () => {
+    return (
+        <div className={s.mainBlock}>
+            <div className={s.greetings}>
+                <span>Hi There</span>
+                <h1>I am Nikolai</h1>
+                <p>A Frontend Developer</p>
+            </div>
+            <div className={s.photo}></div>
+        </div>
+    );
+}
+
+export default Main;
