6cdfda24fcf1d8f729904f8cc266cf98010632d6
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-01-10 12:42:21 +0300

    completed stage 1 portfolio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,14 @@ const Footer = () => {
     return (
         <div className={s.footerBlock}>
             <div className={c.content}>
-
+                <h3>Nikolai Doroshenko</h3>
+                <div className={s.contentBlock}>
+                    <div className={s.iconContainer}></div>
+                    <div className={s.iconContainer}></div>
+                    <div className={s.iconContainer}></div>
+                    <div className={s.iconContainer}></div>
+                </div>
+                <p>© All rights reserved</p>
             </div>
         </div>
     );
