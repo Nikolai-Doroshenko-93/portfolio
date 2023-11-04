bd90393eb7f285f2766ae142dfd128f706228523
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-11-04 14:03:51 +0300

    add neon text

---
+++ src/newComponents/neonText/Neon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Neon.css'
+
+function Neon() {
+
+    return (
+        <div className="App">
+            <div id="neon">Neon</div>
+        </div>
+    );
+}
+
+export default Neon;
