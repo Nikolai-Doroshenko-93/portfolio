5f78583cefa3221b0e9511db9256df59930cac00
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-11-15 14:37:29 +0300

    add work from cards projects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from "./components/p4-Projects/Projects";
 import RemoteWork from "./components/p5-RemoteWork/RemoteWork";
 import Contacts from "./components/p6-Contacts/Contacts";
 import Footer from "./components/p7-Footer/Footer";
+import Cards from "./newComponents/newCards/Cards";
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         <RemoteWork/>
         <Contacts/>
         <Footer/>
-
+        <Cards/>
     </div>
   );
 }
